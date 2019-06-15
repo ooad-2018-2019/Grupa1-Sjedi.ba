73b7d6c52dc4aa4e1f4de0129c2a422b3b93f757
Dzandza <dtabakovic1@etf.unsa.ba>
2019-06-16 00:11:20 +0200

    javascript

--- Implementacija/SjediBa/SjediBa/Controllers/EventsController.cs
+++ Implementacija/SjediBa/SjediBa/Controllers/EventsController.cs
@@ -17,4 +17,6 @@ namespace SjediBa.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
+    
+    
 }
--- Implementacija/SjediBa/SjediBa/wwwroot/js/site.js
+++ Implementacija/SjediBa/SjediBa/wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function SetCurrentEvent(id, naziv){
+    console.log(id + " " + naziv);
+
+    document.getElementById('eventName').innerHTML = naziv;
+
+}
