c85e86c160012a68b9012af6052f68dfa17a885c
Dzandza <dtabakovic1@etf.unsa.ba>
2019-06-15 22:57:48 +0200

    jos implementacije

--- Implementacija/SjediBa/SjediBa/Controllers/LoginController.cs
+++ Implementacija/SjediBa/SjediBa/Controllers/LoginController.cs
@@ -2,7 +2,7 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using SjediBa.Models;
 
-namespace dotnettest.Controllers
+namespace SjediBa.Controllers
 {
     public class LoginController : Controller
     {
