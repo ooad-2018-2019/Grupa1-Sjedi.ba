45969ae0dc02f1eaed4d6c12593484ba6ab22e29
bakir <bsabljica1@etf.unsa.ba>
2019-06-18 02:56:43 +0200

    jos jos jos jos jos jos jos jos jos jso jso jso jso jso jso previse implementacije

--- Implementacija/SjediBa/SjediBa/Controllers/HomeController.cs
+++ Implementacija/SjediBa/SjediBa/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using SjediBa.Models;
+using System.Linq;
 
 namespace SjediBa.Controllers
 {
@@ -9,6 +10,40 @@ namespace SjediBa.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Test(string email, string password)
+        {
+
+            if (email.Length == null || password.Length == null || email.Length == 0 || password.Length == 0)
+                return RedirectToAction("Index");
+
+            using (var db = new DatabaseContext())
+            {
+
+                RegisteredUserModel r = db.Registrovani.Where(e => e.Name == email && e.password == password).FirstOrDefault();
+
+                UnregistredUserModel ur = db.Neregistrovani.Where(e => e.Name == email && e.password == password).FirstOrDefault();
+
+                OrganizerModel o = db.Oranizatori.Where(e => e.Name == email && e.password == password).FirstOrDefault();
+
+                LocalAdministratorModel lol = db.Lokalni.Where(e => e.Name == email && e.Password == password).FirstOrDefault();
+
+                MainAdministratorModel mom = db.Glavni.Where(e => e.Name == email && e.Password == password).FirstOrDefault();
+
+                if (r == null && ur == null && o == null && lol == null && mom == null)
+                    return RedirectToAction("Index");
+
+                if (r != null)
+                    return View("../Events/Events");
+
+                return RedirectToAction("Index");
+
+
+            }
+
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- Implementacija/SjediBa/SjediBa/Controllers/LoginController.cs
+++ Implementacija/SjediBa/SjediBa/Controllers/LoginController.cs
@@ -1,14 +1,54 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using SjediBa.Models;
+using System.Linq;
 
 namespace SjediBa.Controllers
 {
     public class LoginController : Controller
     {
+
+        //private DatabaseContext db;
+
         public IActionResult Login()
         {
             return View();
+        }
+
+        public IActionResult Test(string email, string password)
+        {
+
+            if (email.Length == null || password.Length == null || email.Length == 0 || password.Length == 0)
+                return RedirectToAction("Login");
+
+            using (var db = new DatabaseContext())
+            {
+
+               RegisteredUserModel r = db.Registrovani.Where(e => e.Name == email && e.password == password).FirstOrDefault();
+
+                UnregistredUserModel ur = db.Neregistrovani.Where(e => e.Name == email && e.password == password).FirstOrDefault();
+
+                OrganizerModel o = db.Oranizatori.Where(e => e.Name == email && e.password == password).FirstOrDefault();
+
+                LocalAdministratorModel lol = db.Lokalni.Where(e => e.Name == email && e.Password == password).FirstOrDefault();
+
+                MainAdministratorModel mom = db.Glavni.Where(e => e.Name == email && e.Password == password).FirstOrDefault();
+
+                if (r == null && ur == null && o == null && lol == null && mom == null)
+                    return RedirectToAction("Login");
+
+                if (r != null)
+                    return View("../Events/Events");
+
+                return RedirectToAction("Login");
+
+
+            }
+
+
+
+           
+            
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
