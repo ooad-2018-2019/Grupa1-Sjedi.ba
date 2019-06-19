bf5a708f81a50fc359bdc0e423fbad63e155e432
Dzandza <dtabakovic1@etf.unsa.ba>
2019-06-19 16:29:29 +0200

    jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos jos implementacije

--- Implementacija/SjediBa/SjediBa/Controllers/SpaceController.cs
+++ Implementacija/SjediBa/SjediBa/Controllers/SpaceController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -12,6 +13,7 @@ namespace SjediBa.Controllers
     public class SpaceController : Controller
     {
         private readonly DatabaseContext _context;
+        private static List<SectionModel> SectionModels = new List<SectionModel>();
 
         public SpaceController(DatabaseContext context)
         {
@@ -21,6 +23,8 @@ namespace SjediBa.Controllers
         // GET: Space/Create
         public IActionResult Create()
         {
+            ViewData["role"] = HttpContext.Session.GetString("role");
+            ViewData["id"] = HttpContext.Session.GetInt32("id");
             ViewData["LocalAdministratorModelId"] = new SelectList(_context.Lokalni, "AdministratorModelId", "Discriminator");
             return View();
         }
@@ -30,18 +34,99 @@ namespace SjediBa.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("SpaceModelId,Name,Address,LocalAdministratorModelId")] SpaceModel spaceModel)
+        public async Task<IActionResult> Create([Bind("SpaceModelId,Name,Address,LocalAdministratorModelId")] SpaceModel spaceModel, string AdminName, string Surename, string Date, string AdminAddress, string Username, string Password, string Password2)
         {
+            ViewData["role"] = HttpContext.Session.GetString("role");
+            ViewData["id"] = HttpContext.Session.GetInt32("id");
+            
             if (ModelState.IsValid)
             {
-                _context.Add(spaceModel);
-                await _context.SaveChangesAsync();
-                return RedirectToAction("Index", "Home");
+
+                
+                    if(DateTime.Parse(Date).CompareTo(DateTime.Now) >= 0 || DateTime.Parse(Date).Year < 1900)
+                        return View();
+                    
+                    if(Password != Password2)
+                        return View();
+                    
+                    RegisteredUserModel registered = _context.Registrovani.Where(e => e.Username == Username).FirstOrDefault();
+
+                    UnregistredUserModel unregistred = _context.Neregistrovani.Where(e => e.Username == Username).FirstOrDefault();
+
+                    OrganizerModel organizer = _context.Oranizatori.Where(e => e.Username == Username).FirstOrDefault();
+
+                    LocalAdministratorModel local = _context.Lokalni.Where(e => e.Username == Username).FirstOrDefault();
+
+                    MainAdministratorModel main = _context.Glavni.Where(e => e.Username == Username).FirstOrDefault();
+
+                    if (!(registered == null && unregistred == null && organizer == null && local == null && main == null))
+                        return View();
+                
+                    
+                    if(SectionModels.Count == 0) return View();
+                    
+                    LocalAdministratorModel localAdministratorModel = new LocalAdministratorModel();
+                    localAdministratorModel.Name = AdminName;
+                    localAdministratorModel.Surname = Surename;
+                    localAdministratorModel.Username = Username;
+                    localAdministratorModel.Password = Password;
+                    localAdministratorModel.Address = AdminAddress;
+                    localAdministratorModel.DateOfBirth = DateTime.Parse(Date);
+
+                    _context.Add(localAdministratorModel);
+                    await _context.SaveChangesAsync();
+
+                    spaceModel.LocalAdministratorModelId = localAdministratorModel.AdministratorModelId;
+                    spaceModel.LocalAdministratorModel = localAdministratorModel;
+                    
+                    _context.Add(spaceModel);
+                    await _context.SaveChangesAsync();
+
+                    foreach (SectionModel sectionModel in SectionModels)
+                    {
+                        sectionModel.SpaceModelId = spaceModel.SpaceModelId;
+                        _context.Add(sectionModel);
+                        await _context.SaveChangesAsync();
+                        foreach (SeatModel seatModel in sectionModel.SeatModels)
+                        {
+                            
+                            seatModel.SectionModelId = sectionModel.SectionModelId;
+                            _context.Add(seatModel);
+                            await _context.SaveChangesAsync();
+                        }
+                        
+                    }
+                    
+                    SectionModels.Clear();    
+                    return RedirectToAction("Index", "Home");
+                
+                
             }
             ViewData["LocalAdministratorModelId"] = new SelectList(_context.Lokalni, "AdministratorModelId", "Discriminator", spaceModel.LocalAdministratorModelId);
             return View(spaceModel);
         }
 
+
+        public IActionResult AddSection(string SectorName, string SeatPrice, string NumberOfSeats)
+        {
+            if(Int32.Parse(NumberOfSeats) < 1 || Int32.Parse(SeatPrice) < 1)
+                return RedirectToAction("Create");
+            
+            SectionModels.Add(new SectionModel());
+            SectionModels[SectionModels.Count - 1].Name = SectorName;
+            SectionModels[SectionModels.Count - 1].SeatPrices = Int32.Parse(SeatPrice);
+            SectionModels[SectionModels.Count - 1].SeatModels = new List<SeatModel>();
+
+            for (int i = 0; i < Int32.Parse(NumberOfSeats); i++)
+            {
+                SectionModels[SectionModels.Count - 1].SeatModels.Add(new SeatModel());
+                SectionModels[SectionModels.Count - 1].SeatModels
+                        .ElementAt(SectionModels[SectionModels.Count - 1].SeatModels.Count - 1).RowSeat =
+                    SectorName + " Sjediste " + (i + 1);
+            }
+
+            return RedirectToAction("Create");
+        }
         // GET: Space/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
